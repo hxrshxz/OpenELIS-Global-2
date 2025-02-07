ac92138483dfcda455dbccf838ffa5e674e5862d
hxrshxz <harshmastic@gmail.com>
2025-02-07 15:05:37 +0530

    conifig files

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'wnwv9q',
+  // projectId: 'unde',
   defaultCommandTimeout: 8000,
   viewportWidth: 1200,
   viewportHeight: 700,
@@ -10,20 +12,24 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       config.specPattern = [
-        "cypress/e2e/login.cy.js",
-        "cypress/e2e/home.cy.js",
-        "cypress/e2e/patientEntry.cy.js",
-        "cypress/e2e/orderEntity.cy.js",
-        "cypress/e2e/workplan.cy.js",
-        "cypress/e2e/nonConform.cy.js",
-        "cypress/e2e/result.cy.js",
-        "cypress/e2e/validation.cy.js",
-        "cypress/e2e/modifyOrder.cy.js",
-        "cypress/e2e/report.cy.js",
-        "cypress/e2e/batchOrderEntry.cy.js",
-        "cypress/e2e/dashboard.cy.js",
-        "cypress/e2e/labNumberManagement.cy.js",
-        "cypress/e2e/AdminE2E/MenuConfig/globalMenuConfig.cy.js",
+        // "cypress/e2e/login.cy.js",
+        // "cypress/e2e/home.cy.js",
+        // "cypress/e2e/batchOrderEntry.cy.js",
+
+
+        // "cypress/e2e/patientEntry.cy.js", //failing
+        "cypress/e2e/orderEntity.cy.js", //failing
+        // "cypress/e2e/workplan.cy.js",//failing
+        // "cypress/e2e/nonConform.cy.js",//failing 
+        // "cypress/e2e/result.cy.js",  //failing
+        // "cypress/e2e/validation.cy.js",//failing
+        // "cypress/e2e/modifyOrder.cy.js",//failing
+        // "cypress/e2e/report.cy.js", // Failing
+        // "cypress/e2e/dashboard.cy.js",  //failing ---> bahati working
+
+
+        // "cypress/e2e/AdminE2E/MenuConfig/globalMenuConfig.cy.js",
+        // "cypress/e2e/labNumberManagement.cy.js",
       ];
       return config;
     },
