91db3b4100a2f8fc30d7285429a665edbf6469f8
hxrshxz <harshmastic@gmail.com>
2025-02-07 15:01:05 +0530

    logs removed

--- frontend/cypress/pages/OrderEntityPage.js
+++ frontend/cypress/pages/OrderEntityPage.js
@@ -35,7 +35,7 @@ class OrderEntityPage {
     cy.intercept("GET", `**/rest/SampleEntryAccessionNumberValidation**`).as(
       "accessionNoValidation",
     );
-    cy.get("#labNo").type(order, { delay: 300 });
+    cy.get("#display_labNo").type(order, { delay: 300 });
 
     cy.wait("@accessionNoValidation").then((interception) => {
       const responseBody = interception.response.body;
